3aca4e59f597d9546d87b1c2d10fbefc149f0a29
kamral <kamral010101@gmail.com>
2020-12-21 13:03:28 +0300

    change all models

--- student/models.py
+++ student/models.py
@@ -4,18 +4,57 @@ from user.models import User
 
 class PhoneNmuber(models.Model):
     phone_number=models.PositiveIntegerField(null=True,blank=True)
+    def __str__(self):
+        return self.phone_number
+
+
+    class Meta:
+        verbose_name='Номер телефона'
+        verbose_name_plural='Номера телефонов'
 
 class Course(models.Model):
     kurs_number=models.PositiveIntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return self.kurs
+
+
+    class Meta:
+        verbose_name='Курс'
+        verbose_name_plural='Курсы'
+
 class Faculty(models.Model):
     faculty_number=models.PositiveIntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.faculty_number
+
+
+    class Meta:
+        verbose_name='Факультет'
+        verbose_name_plural='Факультеты'
 
 class Direction(models.Model):
     direction_name=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.direction_name
+
+
+    class Meta:
+        verbose_name='Направление'
+        verbose_name_plural='Направления'
+
 class Room(models.Model):
     room_number=models.PositiveIntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.room_number
+
+
+    class Meta:
+        verbose_name='Номер комнаты'
+        verbose_name_plural='Номера комнат'
 
 
 class student(models.Model):
@@ -29,5 +68,15 @@ class student(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     direction = models.ForeignKey(Direction, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user
+
+
+    class Meta:
+        verbose_name='Студент'
+        verbose_name_plural='Студенты'
+
 
 
